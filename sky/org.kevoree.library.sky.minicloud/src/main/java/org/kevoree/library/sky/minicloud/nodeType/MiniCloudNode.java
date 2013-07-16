ff1b73ada6326cc099a44ca81c5cc263f9d49c26
Erwan Daubert <erwan.daubert@gmail.com>
2013-07-16 17:17:19 +0200

    add ip for child (if the host have one)

--- sky/org.kevoree.library.sky.minicloud/src/main/java/org/kevoree/library/sky/minicloud/nodeType/MiniCloudNode.java
+++ sky/org.kevoree.library.sky.minicloud/src/main/java/org/kevoree/library/sky/minicloud/nodeType/MiniCloudNode.java
@@ -1,6 +1,13 @@
 package org.kevoree.library.sky.minicloud.nodeType;
 
+import org.kevoree.ContainerNode;
+import org.kevoree.ContainerRoot;
 import org.kevoree.annotation.*;
+import org.kevoree.api.service.core.handler.UUIDModel;
+import org.kevoree.cloner.ModelCloner;
+import org.kevoree.framework.Constants;
+import org.kevoree.framework.KevoreePlatformHelper;
+import org.kevoree.framework.KevoreePropertyHelper;
 import org.kevoree.library.defaultNodeTypes.JavaSENode;
 import org.kevoree.library.sky.api.CloudNode;
 import org.kevoree.library.sky.api.KevoreeNodeRunnerFactory;
@@ -11,6 +18,11 @@ import org.kevoree.library.sky.api.execution.KevoreeNodeRunner;
 import org.kevoree.library.sky.api.planning.PlanningManager;
 import org.kevoree.library.sky.minicloud.MiniCloudKevoreeNodeRunner;
 import org.kevoree.log.Log;
+
+import java.lang.management.ManagementFactory;
+import java.util.List;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * User: Erwan Daubert - erwan.daubert@gmail.com
@@ -32,6 +44,8 @@ public class MiniCloudNode extends JavaSENode implements CloudNode, PaaSNode {
 
     private KevoreeNodeManager nodeManager;
 
+    private ScheduledThreadPoolExecutor executor = null;
+
     @Start
     @Override
     public void startNode() {
@@ -41,12 +55,16 @@ public class MiniCloudNode extends JavaSENode implements CloudNode, PaaSNode {
         kompareBean = new PlanningManager(this);
         mapper = new CommandMapper(nodeManager);
         mapper.setNodeType(this);
+
+        executor = new ScheduledThreadPoolExecutor(ManagementFactory.getOperatingSystemMXBean().getAvailableProcessors());
+        executor.scheduleAtFixedRate(new IPManager(this), 15, 15, TimeUnit.SECONDS);
     }
 
     @Stop
     @Override
     public void stopNode() {
         Log.debug("Stopping node type of {}", this.getName());
+        executor.shutdownNow();
         nodeManager.stop();
         super.stopNode();
     }
@@ -55,6 +73,43 @@ public class MiniCloudNode extends JavaSENode implements CloudNode, PaaSNode {
         @Override
         public KevoreeNodeRunner createKevoreeNodeRunner(String nodeName) {
             return new MiniCloudKevoreeNodeRunner(nodeName, MiniCloudNode.this);
+        }
+    }
+
+    public class IPManager implements Runnable {
+        private MiniCloudNode node;
+
+        public IPManager(MiniCloudNode node) {
+            this.node = node;
+        }
+
+        public synchronized void run() {
+            UUIDModel uuidModel = node.getModelService().getLastUUIDModel();
+            ModelCloner cloner = new ModelCloner();
+            ContainerRoot readWriteModel = cloner.clone(node.getModelService().getLastModel());
+
+            boolean update = false;
+
+            ContainerNode nodeInstance = node.getModelElement();
+            List<String> hostIps = KevoreePropertyHelper.instance$.getNetworkProperties(node.getModelService().getLastModel(), nodeInstance.getName(), Constants.instance$.getKEVOREE_PLATFORM_REMOTE_NODE_IP());
+
+            if (hostIps.size() > 0) {
+                Log.debug("The host {} has some ips so if its childs don't have at least one, we add all of them as ip of the childs");
+                for (ContainerNode child : nodeInstance.getHosts()) {
+                    List<String> ips = KevoreePropertyHelper.instance$.getNetworkProperties(node.getModelService().getLastModel(), child.getName(), Constants.instance$.getKEVOREE_PLATFORM_REMOTE_NODE_IP());
+                    if (ips.size() <= 0) {
+                        for (String ip : hostIps) {
+                            Log.debug("Adding {} as IP for {}", ip, child.getName());
+                            KevoreePlatformHelper.instance$.updateNodeLinkProp(readWriteModel, child.getName(), child.getName(), org.kevoree.framework.Constants.instance$.getKEVOREE_PLATFORM_REMOTE_NODE_IP(), ip, "LAN", 100);
+                            update = true;
+                        }
+                    }
+                }
+            }
+
+            if (update) {
+                node.getModelService().compareAndSwapModel(uuidModel, readWriteModel);
+            }
         }
     }
 }
