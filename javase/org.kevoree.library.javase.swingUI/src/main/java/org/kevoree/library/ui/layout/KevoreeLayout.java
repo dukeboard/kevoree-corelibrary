342326b304ce166e1d05954968a3efc0d79eef4e
Erwan Daubert <erwan.daubert@gmail.com>
2013-08-29 09:31:03 +0200

    fix null pointer on stop

--- javase/org.kevoree.library.javase.swingUI/src/main/java/org/kevoree/library/ui/layout/KevoreeLayout.java
+++ javase/org.kevoree.library.javase.swingUI/src/main/java/org/kevoree/library/ui/layout/KevoreeLayout.java
@@ -9,7 +9,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.beans.PropertyChangeListener;
 import java.beans.VetoableChangeListener;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
 /**
@@ -45,9 +44,9 @@ public class KevoreeLayout {
                 public void run() {
                     content = new JPanel();
                     content.setLayout(new BorderLayout());
-                    frame = new JFrame("Kevoree node : "+System.getProperty("node.name"));
+                    frame = new JFrame("Kevoree node : " + System.getProperty("node.name"));
                     frame.getRootPane().putClientProperty("apple.awt.brushMetalLook", Boolean.TRUE);
-                    p = new JChromeTabbedPane("osx",content);
+                    p = new JChromeTabbedPane("osx", content);
                     p.setPreferredSize(new Dimension(50, 40));
                     p.getSize(p.getPreferredSize());
                     if (System.getProperty("os.name").toUpperCase().startsWith("MAC")) {
@@ -69,8 +68,6 @@ public class KevoreeLayout {
     private void release() {
 
 
-
-
         content.removeAll();
         frame.remove(content);
         frame.setVisible(false);
@@ -78,7 +75,6 @@ public class KevoreeLayout {
         frame = null;
         p = null;
         content = null;
-
 
 
     }
@@ -102,59 +98,59 @@ public class KevoreeLayout {
         //DO RELEASE
         if (p != null && tabs.containsKey(key)) {
             p.closeTab(tabs.get(key));
-        }
 
-        //RELEASE LISTENER
-        JComponent subPanel = tabs.get(key).getInternPanel();
-        content.remove(subPanel);
+            //RELEASE LISTENER
+            JComponent subPanel = tabs.get(key).getInternPanel();
+            content.remove(subPanel);
 
-        AncestorListener[] listeners = subPanel.getAncestorListeners();
-        for (AncestorListener listener : listeners) {
-            subPanel.removeAncestorListener(listener);
-        }
-        VetoableChangeListener[] listeners2 = subPanel.getVetoableChangeListeners();
-        for (VetoableChangeListener listener : listeners2) {
-            subPanel.removeVetoableChangeListener(listener);
-        }
-        ComponentListener[] listeners3 = subPanel.getComponentListeners();
-        for (ComponentListener listener : listeners3) {
-            subPanel.removeComponentListener(listener);
-        }
-        FocusListener[] listeners4 = subPanel.getFocusListeners();
-        for (FocusListener listener : listeners4) {
-            subPanel.removeFocusListener(listener);
-        }
-        HierarchyBoundsListener[] listeners5 = subPanel.getHierarchyBoundsListeners();
-        for (HierarchyBoundsListener listener : listeners5) {
-            subPanel.removeHierarchyBoundsListener(listener);
-        }
-        HierarchyListener[] listeners6 = subPanel.getHierarchyListeners();
-        for (HierarchyListener listener : listeners6) {
-            subPanel.removeHierarchyListener(listener);
-        }
-        InputMethodListener[] listeners7 = subPanel.getInputMethodListeners();
-        for (InputMethodListener listener : listeners7) {
-            subPanel.removeInputMethodListener(listener);
-        }
-        KeyListener[] listeners8 = subPanel.getKeyListeners();
-        for (KeyListener listener : listeners8) {
-            subPanel.removeKeyListener(listener);
-        }
-        MouseListener[] listeners9 = subPanel.getMouseListeners();
-        for (MouseListener listener : listeners9) {
-            subPanel.removeMouseListener(listener);
-        }
-        MouseMotionListener[] listeners10 = subPanel.getMouseMotionListeners();
-        for (MouseMotionListener listener : listeners10) {
-            subPanel.removeMouseMotionListener(listener);
-        }
-        MouseWheelListener[] listeners11 = subPanel.getMouseWheelListeners();
-        for (MouseWheelListener listener : listeners11) {
-            subPanel.removeMouseWheelListener(listener);
-        }
-        PropertyChangeListener[] listeners12 = subPanel.getPropertyChangeListeners();
-        for (PropertyChangeListener listener : listeners12) {
-            subPanel.removePropertyChangeListener(listener);
+            AncestorListener[] listeners = subPanel.getAncestorListeners();
+            for (AncestorListener listener : listeners) {
+                subPanel.removeAncestorListener(listener);
+            }
+            VetoableChangeListener[] listeners2 = subPanel.getVetoableChangeListeners();
+            for (VetoableChangeListener listener : listeners2) {
+                subPanel.removeVetoableChangeListener(listener);
+            }
+            ComponentListener[] listeners3 = subPanel.getComponentListeners();
+            for (ComponentListener listener : listeners3) {
+                subPanel.removeComponentListener(listener);
+            }
+            FocusListener[] listeners4 = subPanel.getFocusListeners();
+            for (FocusListener listener : listeners4) {
+                subPanel.removeFocusListener(listener);
+            }
+            HierarchyBoundsListener[] listeners5 = subPanel.getHierarchyBoundsListeners();
+            for (HierarchyBoundsListener listener : listeners5) {
+                subPanel.removeHierarchyBoundsListener(listener);
+            }
+            HierarchyListener[] listeners6 = subPanel.getHierarchyListeners();
+            for (HierarchyListener listener : listeners6) {
+                subPanel.removeHierarchyListener(listener);
+            }
+            InputMethodListener[] listeners7 = subPanel.getInputMethodListeners();
+            for (InputMethodListener listener : listeners7) {
+                subPanel.removeInputMethodListener(listener);
+            }
+            KeyListener[] listeners8 = subPanel.getKeyListeners();
+            for (KeyListener listener : listeners8) {
+                subPanel.removeKeyListener(listener);
+            }
+            MouseListener[] listeners9 = subPanel.getMouseListeners();
+            for (MouseListener listener : listeners9) {
+                subPanel.removeMouseListener(listener);
+            }
+            MouseMotionListener[] listeners10 = subPanel.getMouseMotionListeners();
+            for (MouseMotionListener listener : listeners10) {
+                subPanel.removeMouseMotionListener(listener);
+            }
+            MouseWheelListener[] listeners11 = subPanel.getMouseWheelListeners();
+            for (MouseWheelListener listener : listeners11) {
+                subPanel.removeMouseWheelListener(listener);
+            }
+            PropertyChangeListener[] listeners12 = subPanel.getPropertyChangeListeners();
+            for (PropertyChangeListener listener : listeners12) {
+                subPanel.removePropertyChangeListener(listener);
+            }
         }
 
         tabs.remove(key);
@@ -165,13 +161,12 @@ public class KevoreeLayout {
         }
 
 
-
     }
 
     public static void main(String[] args) {
         getInstance().displayTab(new JLabel("YO"), "YOTab");
         //getInstance().displayTab(new JLabel("ta mere en short"), "MM");
-       // getInstance().displayTab(new JLabel("YO33333"), "fucking");
+        // getInstance().displayTab(new JLabel("YO33333"), "fucking");
     }
 
 }
