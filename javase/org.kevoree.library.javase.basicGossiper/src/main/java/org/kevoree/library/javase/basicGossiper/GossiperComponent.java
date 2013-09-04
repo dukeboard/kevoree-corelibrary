f4e9d71117f9bf73a206c81cf5172a19e52f8a93
Erwan Daubert <erwan.daubert@gmail.com>
2013-09-04 14:17:43 +0200

    update dependencies according to check of deployunit in kevoree

--- javase/org.kevoree.library.javase.basicGossiper/src/main/java/org/kevoree/library/javase/basicGossiper/GossiperComponent.java
+++ javase/org.kevoree.library.javase.basicGossiper/src/main/java/org/kevoree/library/javase/basicGossiper/GossiperComponent.java
@@ -6,7 +6,10 @@ import org.kevoree.annotation.DictionaryType;
 import java.util.List;
 
 @DictionaryType({
-        @DictionaryAttribute(name = "interval", defaultValue = "30000", optional = true)
+        @DictionaryAttribute(name = "interval", defaultValue = "30000", optional = true),
+        @DictionaryAttribute(name = "port", defaultValue = "8000", optional = true, fragmentDependant = true),
+        @DictionaryAttribute(name = "ip", defaultValue = "0.0.0.0", optional = true, fragmentDependant = true),
+        @DictionaryAttribute(name = "ssl", defaultValue = "false", vals = {"true", "false"})
 })
 public interface GossiperComponent {
 
